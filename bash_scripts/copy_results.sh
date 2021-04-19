0777716c7cfb936755b5f02963f10ba844fd9dd4
Wajdi Halabi <joehal1998@gmail.com>
2021-04-19 10:43:13 -0400

    copy_results.sh

---
+++ bash_scripts/copy_results.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo 1
+cp -R ../../results/June_22_17_59_03_node1794_Partition /zfs/dicelab/ltb2021/results/4_partition_levels_10_devices
+echo 2
+cp -R ../../results/June_22_12_24_06_node1794_Partition /zfs/dicelab/ltb2021/results/4_partition_levels_1_device
+echo 3
+cp -R ../../results/June_30_02_13_00_node1791_Partition /zfs/dicelab/ltb2021/results/4_partition_levels_1_device_run2
+echo 4
+cp -R ../../results/June_16_16_48_10_node1985_Partition /zfs/dicelab/ltb2021/results/partions_1_device
+echo 5
+cp -R ../../results/June_16_20_51_59_node1839_Partition /zfs/dicelab/ltb2021/results/partions_10_devices
+echo 6
+cp -R ../../results/June_24_16_24_44_ip-10-0-1-201_Partition /zfs/dicelab/ltb2021/results/partionns_AWS
+echo 7
+cp -R ../../results/June_04_08_15_24_node1709 /zfs/dicelab/ltb2021/results/three_node
+echo 8
+cp -R ../../results/July_15_23_33_34_node1732_msg_size /zfs/dicelab/ltb2021/results/10_devices_10_message_sizes
+echo 9
+cp -R ../../results/July_12_13_47_30_node1733 /zfs/dicelab/ltb2021/results/3_editions_10_devices_4_message_sizes_run_2
+echo 10
+cp -R ../../results/July_23_16_48_08_node0229_Partition /zfs/dicelab/ltb2021/results/4_partition_levels_1000_devices
+echo 11
+cp -R ../../results/July_17_10_multi_node /zfs/dicelab/ltb2021/results/full_multi_node
+echo 12
+cp -R ../../results/July_18_22_29_10_node2052_2units /zfs/dicelab/ltb2021/results/full_multi_node_2_units
+echo 13
+cp -R ../../results/July_14_11_14_55_node0199 /zfs/dicelab/ltb2021/results/all_units_multi_node
+echo 14
+cp -R ../../results/July_20_09_32_55_node1749_Pareto /zfs/dicelab/ltb2021/results/pareto_dist
+echo DONE
